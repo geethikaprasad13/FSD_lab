414654be9175960915c21e8918cfb6d6ba7025e6
geethikaprasad13 <geethikaprasad13@gmail.com>
2025-01-08 07:44:54 +0530

    Add Student_grade pgm

---
+++ Grades.js
@@ -0,0 +1,17 @@
+function Student(name, grade) {
+    this.name = name;
+    this.grade = grade;
+}
+
+Student.prototype.study = function () {
+    console.log(`${this.name} is studying.`);
+};
+
+Student.prototype.getGrade = function () {
+    return this.grade;
+};
+
+// Example
+const student1 = new Student("Alice", "A");
+student1.study();
+console.log(student1.getGrade());
