9411f80ee5e40738f172a25e3e22101f1fec1f99
geethikaprasad13 <geethikaprasad13@gmail.com>
2025-01-08 07:56:15 +0530

    Added html file

--- variables.js
+++ variables.js
@@ -1,18 +1,7 @@
-// Using var, let, and const
-var num = 10; // function-scoped
-let str = "Hello"; // block-scoped
-const isTrue = true; // block-scoped and cannot be reassigned
+var num = 10;
+let str = "Hello";
+const isTrue = true;
 
-console.log("Before reassignment:");
-console.log("var num:", num);
-console.log("let str:", str);
-console.log("const isTrue:", isTrue);
-
-// Reassigning variables
-num = 20;
-str = "Hi";
-// isTrue = false; // Uncommenting this will cause an error
-
-console.log("After reassignment:");
-console.log("var num:", num);
-console.log("let str:", str);
+console.log("Var:", num);
+console.log("Let:", str);
+console.log("Const:", isTrue);
